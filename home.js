641f4fa355a12ff85e66ff0df6f2edadb9a5b873
Niculinha <137734825+Niculinha@users.noreply.github.com>
2023-12-01 18:48:27 -0300

    Update home.js

--- home.js
+++ home.js
@@ -1,16 +1,1 @@
-document.getElementById('form-busca').addEventListener('submit', function (e) {
-    const inputBusca = document.getElementById('busca');
-    const termoBusca = inputBusca.value.toLowerCase();
-  
-    if (termoBusca.includes('lasanha')) {
-      // Se o termo de busca contém "lasanha", redirecione para 'receitasalgadas.html'
-      window.location.href = 'receitasalgadas.html';
-    } else if (termoBusca.includes('bolinho de chuva')) {
-      // Se o termo de busca contém "bolinho de chuva", redirecione para 'receitadoces.html'
-      window.location.href = 'receitadoces.html';
-    } else {
-      
-      e.preventDefault();
-      alert('Termo de busca não reconhecido.');
-    }
-  });
+
